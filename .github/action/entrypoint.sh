4cb497476506f449700c7881dee9fdfd79cd70ee
Emanuel Lima <emanuel-lima@outlook.com>
2021-03-29 13:14:08 -0300

    Fix Menu Creation Action

--- .github/action/entrypoint.sh
+++ .github/action/entrypoint.sh
@@ -6,7 +6,7 @@ export PATH="/github/home/.local/bin:$PATH"
 
 apt-get update && apt-get install -y pandoc \
     texlive-xetex texlive-fonts-recommended \
-    texlive-generic-recommended
+    texlive-generic-recommended inkscape build-essential
 
 pip3 install --user --no-cache-dir -r requirements.txt
 
