3168e9da9c47df2dab7e6593a9d4992180a3dfe3
Emanuel Lima <emanuel-lima@outlook.com>
2021-08-05 14:49:46 -0300

    Fix sqlalchemy action bug

--- .github/action/entrypoint.sh
+++ .github/action/entrypoint.sh
@@ -7,9 +7,10 @@ export PATH="/github/home/.local/bin:$PATH"
 apt-get update && apt-get install -y pandoc \
     texlive-xetex texlive-fonts-recommended \
     texlive-generic-recommended inkscape build-essential \
-    python-dev python3-dev
+    python-dev python3-dev libpq-dev
 
 pip3 install --user --no-cache-dir -r requirements.txt
+pip3 install --user --no-cache-dir pystan
 pip3 install --user --no-cache-dir prophet
 
 cd notebooks
